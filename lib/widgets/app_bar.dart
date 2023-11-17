9ab5c6e7b434cce55842d53517793f9a4bb9dfde
BeeibekKunwar <beeibek098765@gmail.com>
2023-11-17 23:35:16 +0545

    AppBar with title, dropdown icon, profile image

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -1,26 +1,48 @@
 import 'package:flutter/material.dart';
 
 AppBar buildAppBar() => AppBar(
-      leading: RichText(
-        text: const TextSpan(
-          children: [
-            TextSpan(
-              text: 'Explore Clothes',
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 14,
-              ),
+      toolbarHeight: 100,
+      title: const Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Explore clothes',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
             ),
-             TextSpan(
-              text: 'Collection',
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 14,
-              ),
+          ),
+          Text(
+            'Collection',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
             ),
-          ],
-        ),
+          ),
+        ],
       ),
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.arrow_drop_down_circle_rounded),
+        ),
+        const Padding(
+          padding: EdgeInsets.all(10.0),
+          child: CircleAvatar(
+            radius: 27,
+            backgroundColor: Color.fromARGB(255, 58, 8, 67),
+            child: CircleAvatar(
+              radius: 25,
+              foregroundColor: Colors.white,
+              backgroundImage: AssetImage(
+                'assets/images/model6.jpg',
+              ),
+            ),
+          ),
+        )
+      ],
     );
